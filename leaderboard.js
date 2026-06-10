7d8c2c007d5b491cfa02ef382f24ba6ad11dc4ba
jc29jc <jacob.carlen29@student.tamdistrict.org>
2026-06-10 05:46:18 +0000

    changes

--- leaderboard.js
+++ leaderboard.js
@@ -11,7 +11,7 @@ function searchPlayer() {
             <p>Scoring Average: ${p.scoring}</p>
             <p>Career Earnings: ${p.earnings}</p>
             <p>${p.fact}</p>
-            <div>${p.media}</div>
+            <div style="max-width: 300px; margin: 15px auto;">${p.media}</div>
         `;
     } else {
         alert("Player not found!");
@@ -26,7 +26,7 @@ const players = {
         scoring: "70.12",
         earnings: "$62,800,000+",
         fact: "Won the Master and US open (both majors) in 2015 at just 21 years old.",
-        media: '<iframe width="100%" height="315" src="https://www.youtube.com/embed/n_03pDAnv20" frameborder="0" allowfullscreen></iframe>'
+        media: '<iframe width="100%" height="200" src="https://www.youtube.com/embed/n_03pDAnv20?si=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     "tiger woods": {
         name: "Tiger Woods",
@@ -35,7 +35,7 @@ const players = {
         scoring: "68.84 (Career Prime)",
         earnings: "$121,000,000+",
         fact: "Held all 4 major trophies at the exact same time in 2001, known as the 'Tiger Slam'.",
-        media: '<iframe width="100%" height="315" src="https://www.youtube.com/watch?v=pq-L5SqXn24" frameborder="0" allowfullscreen></iframe>'
+        media: '<iframe width="100%" height="200" src="https://www.youtube.com/embed/Y0n6UInwH-0?si=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     "phil mickelson": {
         name: "Phil Mickelson",
@@ -44,7 +44,7 @@ const players = {
         scoring: "69.58",
         earnings: "$96,500,000+",
         fact: "The oldest player in golf history to win a Major (2021 PGA Championship at age 50).",
-        media: '<iframe width="100%" height="315" src="https://www.youtube.com/embed/9wJ_m_gG6lA" frameborder="0" allowfullscreen></iframe>'
+        media: '<iframe width="100%" height="200" src="https://www.youtube.com/embed/9wJ_m_gG6lA?si=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     "rory mcilroy": {
         name: "Rory McIlroy",
@@ -53,7 +53,7 @@ const players = {
         scoring: "69.25",
         earnings: "$90,100,000+",
         fact: "One of only three players to ever win four majors by the age of 25.",
-        media: '<iframe width="100%" height="315" src="https://www.youtube.com/embed/uCymP4uKzM0" frameborder="0" allowfullscreen></iframe>'
+        media: '<iframe width="100%" height="200" src="https://www.youtube.com/embed/uCymP4uKzM0?si=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     "brooks koepka": {
         name: "Brooks Koepka",
@@ -62,6 +62,6 @@ const players = {
         scoring: "69.80",
         earnings: "$43,000,000+",
         fact: "Successfully won back-to-back US Opens and back-to-back PGA Championships.",
-        media: '<iframe width="100%" height="315" src="https://www.youtube.com/embed/8I0f0z-vE4k" frameborder="0" allowfullscreen></iframe>'
+        media: '<iframe width="100%" height="200" src="https://www.youtube.com/embed/8I0f0z-vE4k?si=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     }
 };
